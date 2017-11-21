949e9a07eee39560a002fa29621175a9c0202c8b
yuki121 <33594019+yuki121@users.noreply.github.com>
2017-11-21 14:17:11 +0900

    Add files via upload

---
+++ mobile.php
@@ -0,0 +1,27 @@
+﻿<!DOCTYPE html>
+<html lang = "ja">
+<head>
+<meta charset = "UTF-8" />
+<meta name = "robots" content = "noindex, nofollow, noarchive" />
+<meta name = "viewport" content = "width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<link href = "bbs8.css" rel = "stylesheet" />
+<title>BBS8</title>
+</head>
+<body>
+<div class = "content">
+<div style = "width:500px;max-width:100%;">
+<div class = "foo">
+<h1>BBS8へようこそ</h1>
+<br />
+<form action = "manual.html" method = "post">
+<input type = "submit" value = "説明と規約" class = "rule" /><br />
+</form>
+<br />
+<form action = "login.php" method = "post">
+<input type = "submit" value = "規約に同意して利用する" /><br />
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ ua_test.php
@@ -0,0 +1,33 @@
+﻿<?php
+$ua = $_SERVER['HTTP_USER_AGENT'];
+if((strpos($ua, 'iPhone') !== false) || (strpos($ua, 'Android') !== false) && (strpos($ua, 'Mobile') !== false)){
+	header('Location: mobile.php');
+	exit();
+}
+?>
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+<meta charset = "UTF-8" />
+<meta name = "robots" content = "noindex, nofollow, noarchive" />
+<link href = "bbs8_design.css" rel = "stylesheet" />
+<title>BBS8</title>
+</head>
+<body>
+<div class = "content">
+<h1>BBS8へようこそ</h1>
+<br />
+<div style = "text-align: center;">
+<form action = "manual.html" method = "post">
+<input type = "submit" value = "説明と規約" /><br />
+</form>
+</div>
+<br />
+<div style = "text-align: center;">
+<form action = "login.php" method = "post">
+<input type = "submit" value = "規約に同意して利用する" /><br />
+</form>
+</div>
+</div>
+</body>
+</html>
